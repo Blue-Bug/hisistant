1c1aac225cf36cedafb02e6633bc87435b110ba2
Blue-bug <kjj9987@gmail.com>
2024-09-06 11:03:13 +0900

    front: tailwind 설정 및 메인페이지 작업

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MainPage from "@/views/MainPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Main',
+      component: MainPage
     },
     {
       path: '/about',
